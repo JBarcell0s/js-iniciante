3dd13e039d528b8c243a2a27ea3e791e071bf235
joao barcellos <joaovitor.b.egea@gmail.com>
2022-04-05 09:41:21 -0300

    primeiros exercicios em js

---
+++ tipos-de-dados/script-dados.js
@@ -0,0 +1,18 @@
+// Declare uma variável contendo uma string
+var aleatoria = "naoSei"
+// Declare uma variável contendo um número dentro de uma string
+var numeroString = "89";
+// Declare uma variável com a sua idade
+var idadeJv = 19;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "joão vitor ",
+    sobrenome = "barcellos egea",
+    nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto)
+// Coloque a seguinte frase em uma variável: It's time
+var fraseTeste = "It's time, to " + nome + "win the game"
+console.log(fraseTeste)
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome)
---
+++ variaveis/script.js
@@ -0,0 +1,12 @@
+var nome = "joão barcellos",
+    idade = 19;
+console.log(nome, idade);
+var comida;
+comida = "rayssa"
+console.log(comida);
+var sabor;
+var time;
+var camisa;
+var tipo;
+var redeSocial;
+console.log(sabor, time, camisa, tipo, redeSocial);
